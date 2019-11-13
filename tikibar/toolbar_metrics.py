ebcfa556b77853294bb3f79acb9f8d6382fc62ab
miguelsanchez-eb <miguel.sanchez@eventbrite.com>
2019-11-13 16:06:30 +0100

    Default parameters

--- tikibar/toolbar_metrics.py
+++ tikibar/toolbar_metrics.py
@@ -81,14 +81,7 @@ class ToolbarMetricsContainer(object):
             (query_type, val, needs_format, {'d': (start, stop)}, explain_data)
         )
 
-    def add_sql_query_metric(self, query_type, val, start, stop, db_alias=None, args=None):
-
-        if db_alias is None:
-            db_alias = 'default'
-
-        if args is None:
-            args = tuple()
-
+    def add_sql_query_metric(self, query_type, val, start, stop, db_alias='default', args=tuple()):
         with connections[db_alias].cursor() as cursor:
             try:
                 cursor.execute("EXPLAIN {}".format(val), args)
