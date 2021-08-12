239374a4b9a6b10a74a6b52b1226b6482287d0eb
Gabriel Flores <gabrielf@eventbrite.com>
2021-08-12 15:50:44 -0300

    Released Eventbrite Common Utilities Library version 0.3.1
    Changelog Details:
    - Revert "Removed CircleCI" (#50)
    - fixes for py3 compatibility (#49)
    - Removed CircleCI

--- tikibar/version.py
+++ tikibar/version.py
@@ -1,3 +1,3 @@
 from six.moves import map
-__version_info__ = (0, 3, 0)
-__version__ = '-'.join([_f for _f in ['.'.join(map(str, __version_info__[:3])), (__version_info__[3:] or [None])[0]] if _f])
+__version_info__ = (0, 3, 1)
+__version__ = '-'.join(filter(None, ['.'.join(map(str, __version_info__[:3])), (__version_info__[3:] or [None])[0]]))
