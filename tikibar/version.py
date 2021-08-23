7923909872c942ca6a6f8446d9450619940346a9
gabrielf-eb <53191391+gabrielf-eb@users.noreply.github.com>
2021-08-23 11:08:49 -0300

    Released Eventbrite Common Utilities Library version 0.3.3 (#55)
    Changelog Details:
    - [PATCH] Fix BoldKeywordFilter process() params (#54)

--- tikibar/version.py
+++ tikibar/version.py
@@ -1,3 +1,3 @@
 from six.moves import map
-__version_info__ = (0, 3, 2)
+__version_info__ = (0, 3, 3)
 __version__ = '-'.join(filter(None, ['.'.join(map(str, __version_info__[:3])), (__version_info__[3:] or [None])[0]]))
