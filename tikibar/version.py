2710acef9d9e01c24cca598477940d455582a2ac
eyal <eyal@eventbrite.com>
2022-12-01 12:11:35 -0800

    Released Eventbrite Common Utilities Library version 0.4.0
    Changelog Details:
    - Move tikibar enabled check to the client for more customization
    - Add setting to skip specific URLs

--- tikibar/version.py
+++ tikibar/version.py
@@ -1,3 +1,3 @@
 from six.moves import map
-__version_info__ = (0, 3, 4)
+__version_info__ = (0, 4, 0)
 __version__ = '-'.join(filter(None, ['.'.join(map(str, __version_info__[:3])), (__version_info__[3:] or [None])[0]]))
